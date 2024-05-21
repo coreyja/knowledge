c891f7dffcecff257709a99ba8a57bbcdc3baa0e
Corey Alexander <coreyja@gmail.com>
2024-05-20 23:19:25 -0400

    Add clippy and ignore the pool var in the cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
-    let db_pool = db::setup_db_pool().await?;
+    let _db_pool = db::setup_db_pool().await?;
 
     println!("Hello, world!");
 
