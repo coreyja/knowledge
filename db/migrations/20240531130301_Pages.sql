65ade289fdc43d149a2db2b174f2fb8c20eab59e
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-05-31 09:41:01 -0400

    made user_id and url unique in pages migration

--- db/migrations/20240531130301_Pages.sql
+++ db/migrations/20240531130301_Pages.sql
@@ -4,5 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE Pages (
     -- page_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES Users (user_id) NOT NULL,
-    url VARCHAR(255) NOT NULL
+    url VARCHAR(255) NOT NULL,
+    UNIQUE (user_id, url)
 );
+
