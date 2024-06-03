f8edbfccf6ed7d0fc1c7de05a9769c8e2579ef54
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-06-03 12:03:05 -0400

    deleted CREATE EXTENSION on the Page.sql

--- db/migrations/20240531130301_Pages.sql
+++ db/migrations/20240531130301_Pages.sql
@@ -1,6 +1,3 @@
--- Ensure the UUID extension is enabled
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE Pages (
     page_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES Users (user_id) NOT NULL,
