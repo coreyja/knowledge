916c2a69369d431d07818506837eaf4dc2e9ecd8
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-06-05 08:07:41 -0400

    fixing some clippy issues

--- db/src/urls.rs
+++ db/src/urls.rs
@@ -1,9 +1,8 @@
+use reqwest;
 pub use sqlx;
 use sqlx::types::chrono;
 pub use sqlx::PgPool;
 use uuid::Uuid;
-use color_eyre::Result;
-use reqwest;
 
 #[derive(sqlx::FromRow, Debug)]
 pub struct Page {
@@ -19,6 +18,13 @@ pub enum AddUrlOutcome {
     Existing(Page),
 }
 
+#[derive(sqlx::FromRow, Debug)]
+pub struct PageSnapShot {
+    pub page_snapshot_id: Uuid,
+    pub page_id: Uuid,
+    pub raw_html: String,
+    pub fetched_at: Option<chrono::DateTime<chrono::Utc>>,
+}
 
 pub async fn add_url(
     pool: &PgPool,
@@ -57,7 +63,6 @@ pub async fn add_url(
     }
 }
 
-
 pub async fn fetch_url_from_pages_table(pool: &PgPool, page_id: Uuid) -> color_eyre::Result<Page> {
     let result = sqlx::query_as!(
         Page,
@@ -70,34 +75,33 @@ pub async fn fetch_url_from_pages_table(pool: &PgPool, page_id: Uuid) -> color_e
     Ok(result)
 }
 
-
 async fn download_raw_html(pool: &PgPool, page_id: Uuid) -> color_eyre::Result<String> {
     let page = fetch_url_from_pages_table(pool, page_id).await?;
-    let response = reqwest::get(&page.url).await.map_err(|e| color_eyre::Report::from(e))?;
-    let html = response.text().await.map_err(|e| color_eyre::Report::from(e))?;
+    let response = reqwest::get(&page.url)
+        .await
+        .map_err(|e| color_eyre::Report::from(e))?;
+    let html = response
+        .text()
+        .await
+        .map_err(|e| color_eyre::Report::from(e))?;
     Ok(html)
 }
 
-#[derive(sqlx::FromRow, Debug)]
-pub struct PageSnapShot {
-    pub page_snapshot_id: Uuid,
-    pub page_id: Uuid,
-    pub raw_html: String,
-    pub fetched_at: Option<chrono::DateTime<chrono::Utc>>,
-}
-
-async fn store_raw_html_in_page_snapshot(pool: &PgPool, page_id: Uuid) -> color_eyre::Result<PageSnapShot> {
+async fn store_raw_html_in_page_snapshot(
+    pool: &PgPool,
+    page_id: Uuid,
+) -> color_eyre::Result<PageSnapShot> {
     let raw_html = download_raw_html(pool, page_id).await?;
     let current_time = chrono::Utc::now();
     let page = fetch_url_from_pages_table(pool, page_id).await?;
-    
+
     let result = sqlx::query_as!(
         PageSnapShot,
         "INSERT INTO PageSnapShot (raw_html, fetched_at, page_id) 
          VALUES ($1, $2, $3) RETURNING page_snapshot_id, page_id, raw_html, fetched_at",
-         raw_html,
-         current_time, 
-         page.page_id
+        raw_html,
+        current_time,
+        page.page_id
     )
     .fetch_one(pool)
     .await?;
@@ -105,7 +109,12 @@ async fn store_raw_html_in_page_snapshot(pool: &PgPool, page_id: Uuid) -> color_
     Ok(result)
 }
 
-pub async fn process_page_snapshot(pool: &PgPool, page_id: Uuid, _user_id: Uuid, _url: &str) -> color_eyre::Result<()> {
+pub async fn process_page_snapshot(
+    pool: &PgPool,
+    page_id: Uuid,
+    _user_id: Uuid,
+    _url: &str,
+) -> color_eyre::Result<()> {
     let outcome = store_raw_html_in_page_snapshot(pool, page_id).await?;
     println!("Outcome: {:?}", outcome);
     Ok(())
