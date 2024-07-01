803f8e41be15dc7523fb6aee7a98fc07c1c7ff00
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-07-01 12:38:10 -0400

    changed insert_article_handler error to the WebResult

--- web/src/users/add_url.rs
+++ web/src/users/add_url.rs
@@ -4,7 +4,7 @@ use axum::{
 };
 use cja::jobs::Job;
 
-use crate::{jobs::process_article::ProcessArticle, AppState};
+use crate::{jobs::process_article::ProcessArticle, AppState, WebResult};
 
 use db::{urls::add_url, users::User};
 use serde::Deserialize;
@@ -19,23 +19,18 @@ pub async fn insert_article_handler(
     State(state): State<AppState>,
     user: User,
     Form(form): Form<ArticleForm>,
-) -> impl IntoResponse {
+) -> WebResult<impl IntoResponse> {
     let url = form.url;
     let user_id = user.user_id;
 
-    match add_url(&state.db, &url, user_id, &true).await {
-        Ok(page) => {
-            let page_id = page.page().page_id;
-            let process_article = ProcessArticle { page_id };
-            process_article
-                .enqueue(state.clone(), "context".to_string())
-                .await
-                .unwrap();
-            Redirect::to(&format!("/article/{page_id}?flash[success]=Article added"))
-        }
-        Err(e) => {
-            eprintln!("Error adding URL: {e:?}");
-            Redirect::to("/dashboard?flash[error]=Could not add article")
-        }
-    }
+    let page = add_url(&state.db, &url, user_id, &true).await?;
+    let page_id = page.page().page_id;
+    let process_article = ProcessArticle { page_id };
+    process_article
+        .enqueue(state.clone(), "insert_article_handler".to_string())
+        .await?;
+
+    Ok(Redirect::to(&format!(
+        "/article/{page_id}?flash[success]=Article added"
+    )))
 }
