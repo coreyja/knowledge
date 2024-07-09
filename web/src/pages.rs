850c0e79b83651d7fd6d9214b896f24f5b1238dc
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-07-09 10:50:08 -0400

    commiting changes

--- web/src/pages.rs
+++ web/src/pages.rs
@@ -189,12 +189,12 @@ pub async fn my_articles(
 
     let categories = sqlx::query!(
         "SELECT DISTINCT c.* 
-     FROM category c
-     JOIN categorymarkdown cm ON c.category_id = cm.category_id
-     JOIN markdown m ON cm.markdown_id = m.markdown_id
-     JOIN pagesnapshot ps ON m.page_snapshot_id = ps.page_snapshot_id
-     JOIN pages p ON ps.page_id = p.page_id
-     WHERE p.user_id = $1",
+        FROM category c
+        JOIN categorymarkdown cm ON c.category_id = cm.category_id
+        JOIN markdown m ON cm.markdown_id = m.markdown_id
+        JOIN pagesnapshot ps ON m.page_snapshot_id = ps.page_snapshot_id
+        JOIN pages p ON ps.page_id = p.page_id
+        WHERE p.user_id = $1",
         user.user_id
     )
     .fetch_all(&state.db)
