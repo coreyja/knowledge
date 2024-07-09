fd5839ca3dba8af9da0abe5606f542216272fac6
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-07-09 15:52:20 -0400

    added functionality to parse titles and diply on the web UI

--- cores/src/page_snapshot.rs
+++ cores/src/page_snapshot.rs
@@ -1,5 +1,7 @@
+use color_eyre::eyre::Result;
 use readability::extractor;
 use reqwest;
+use scraper::{Html, Selector};
 use sqlx::types::chrono;
 use url::Url;
 use uuid::Uuid;
@@ -23,4 +25,15 @@ pub fn clean_raw_html(raw_html: &str, url: &Url) -> color_eyre::Result<String> {
     let mut raw_html_cursor = std::io::Cursor::new(raw_html);
     let article = extractor::extract(&mut raw_html_cursor, url)?;
     Ok(article.content)
+}
+
+pub fn extract_title(content: &str) -> Result<String> {
+    let document = Html::parse_document(content);
+    let title_selector = Selector::parse("title").unwrap();
+
+    if let Some(title_element) = document.select(&title_selector).next() {
+        Ok(title_element.text().collect::<String>())
+    } else {
+        Ok("No title found".to_string())
+    }
 }
